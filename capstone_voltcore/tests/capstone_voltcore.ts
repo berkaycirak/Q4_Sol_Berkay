3f544b163a43991c28726c97f8d3416513b5217a
berkai <berkaycirak_11@hotmail.com>
2024-11-23 17:30:39 +0300

    feat: mpl core .so file is added to the test

--- capstone_voltcore/tests/capstone_voltcore.ts
+++ capstone_voltcore/tests/capstone_voltcore.ts
@@ -1,16 +1,34 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { CapstoneVoltcore } from "../target/types/capstone_voltcore";
+import { MPL_CORE_PROGRAM_ID } from "@metaplex-foundation/mpl-core";
 
 describe("capstone_voltcore", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+	// Configure the client to use the local cluster.
+	anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.CapstoneVoltcore as Program<CapstoneVoltcore>;
+	const program = anchor.workspace
+		.CapstoneVoltcore as Program<CapstoneVoltcore>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+	it("An asset collection is created for the campaign", async () => {
+		const campaignAssetMplCollection = anchor.web3.Keypair.generate();
+
+		// Add your test here.
+		const tx = await program.methods
+			.createCampaignAsset({
+				name: "Test Asset",
+				uri: "https://example.com/asset",
+			})
+			.accountsPartial({
+				payer: anchor.getProvider().publicKey,
+				campaignAssetMplCollection:
+					campaignAssetMplCollection.publicKey,
+				updateAuthority: null,
+				systemProgram: anchor.web3.SystemProgram.programId,
+				mplCoreProgram: MPL_CORE_PROGRAM_ID,
+			})
+			.signers([campaignAssetMplCollection])
+			.rpc();
+		console.log("Your transaction signature", tx);
+	});
 });
